6bf4c8d568c7cd7e34c9e1092c003d15cc469d2b
Riley Harbick <rileyharbick@gmail.com>
2021-09-30 11:52:42 -0400

    add images for index and js

---
+++ open.js
@@ -0,0 +1,8 @@
+const navlinks = document.getElementsByTagName('nav')[0];
+const hamburgerButton = document.getElementById('hamburger-button');
+
+function toggleNavlinks() {
+  navlinks.classList.toggle('open'); 
+}
+
+hamburgerButton.addEventListener('click', toggleNavlinks);
